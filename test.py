c7162e4ff4a7b1a3f1b66a146296597fca4db278
donthor <parockho@cisco.com>
2020-04-08 16:22:38 -0400

    first vs code test

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ print ("this strips the 'h' character",string.strip('h'))
 print ("this makes the string all caps", string.upper())
 
 print (f'this is "hello" with f-strings {string}')
+
+print()
+print()
