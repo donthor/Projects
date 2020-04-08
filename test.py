fc567fab1aafe800da90bfd9c47824e6321ad1c0
donthor <parockho@cisco.com>
2020-04-08 15:56:36 -0400

    testing

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ string = 'hello'
 print ("this strips the 'h' character",string.strip('h'))
 print ("this makes the string all caps", string.upper())
 
+print (f'this is "hello" with f-strings {string}')
