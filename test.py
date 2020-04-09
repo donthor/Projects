6d2ff6d27392fa48d7210de676c2e9d3e743d4f9
donthor <parockho@cisco.com>
2020-04-09 13:20:29 -0400

    added comment line

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 string = 'hello'
 print ("this strips the 'h' character",string.strip('h'))
 print ("this makes the string all caps", string.upper())
+# print ("this makes the string Capital", string.capitalize())
 
 print ('this is hello with format method: {}'.format(string))
 print (f'this is "hello" with f-strings {string}')
