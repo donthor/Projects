dcb9ef89a1723155ffe70644339620cdbf1d28d9
donthor <parockho@cisco.com>
2020-04-09 08:21:36 -0400

    added format method

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ string = 'hello'
 print ("this strips the 'h' character",string.strip('h'))
 print ("this makes the string all caps", string.upper())
 
+print ('this is hello with format method: {}'.format(string))
 print (f'this is "hello" with f-strings {string}')
 
 print()
