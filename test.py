fcce0acb3d0133e9abea903077dbbe587e2b60a5
donthor <parockho@cisco.com>
2020-04-16 18:16:20 -0400

    added slicing

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ print ("this makes the string Capital", string.capitalize())
 
 print ('this is hello with format method: {}'.format(string))
 print (f'this is "hello" with f-strings {string}')
-
+print (f'this is the middle of "hello" = {string[1:4]}')
 print()
 print()
